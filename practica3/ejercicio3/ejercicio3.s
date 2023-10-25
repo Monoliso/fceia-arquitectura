5ddd8f15a05e1916babdbfff674630e27661d919
Santiago <santiago.libonati@gmail.com>
2023-10-25 08:33:08 -0300

    Me trolearon, yo subi el ejercicio 3

---
+++ practica3/ejercicio3/ejercicio3.s
@@ -0,0 +1,37 @@
+.data
+a: .float 0.5, 2.0, 3.2, 4.0, 5.0
+b: .float 9.5, 8.0, 6.8, 6.0, 5.0
+len: .long 5
+
+
+.text
+.global main
+main:
+  leaq a, %rdi
+  leaq b, %rsi
+  xorq %rdx, %rdx
+  movl len, %edx
+  call sum
+
+  ret
+
+
+# sum(rdi = vector float, rsi = vector float, rdx = largo de los vectores)
+# devuelve la suma de a pares de los valores en los vectores en la direccion de rdi
+sum:
+  movq %rdx, %rcx # para usar loop
+  movq $0, %r8
+  cvtsi2ss %r8, %xmm0
+  cvtsi2ss %r8, %xmm1
+sum_loop:
+  movss (%rdi, %r8, 4), %xmm0
+  movss (%rsi, %r8, 4), %xmm1
+
+  addss %xmm1, %xmm0
+
+  movss %xmm0, (%rdi, %r8, 4)
+  incq %r8
+
+  loop sum_loop
+
+  ret
